c4d5d18dc9c1ab61a3c7f73b705a444b7a7a18de
Denis Zuenko <neron.v2@gmail.com>
2020-02-09 10:05:04 +0300

    Update model

--- src/loader.py
+++ src/loader.py
@@ -6,8 +6,8 @@ from torch.utils import data
 class UserDataset(data.Dataset):
 
     def __init__(self, list_of_users, labels, mode='train'):
-        self.labels = labels
         self.list_of_users = list_of_users
+        self.labels = labels
         X_train, y_train, X_test, y_test = train_test_split(self.list_of_users, self.labels, test_size=0.1,
                                                             random_state=42, shuffle=True)
         if mode == 'train':
@@ -18,7 +18,6 @@ class UserDataset(data.Dataset):
             self.labels = y_test
 
     def __len__(self):
-        'Denotes the total number of samples'
         return len(self.list_of_users)
 
     def __getitem__(self, index):
